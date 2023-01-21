ba9cbddb26d24b7e52701b148571b91faa5ed17d
Tutul Kabir <tutulkabir@gmail.com>
2023-01-21 15:34:11 +0530

    creating app.js file and import in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-    <h1>Hello World</h1>
+    <App />
   </div>
 );
