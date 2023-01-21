0d333c14ad6387ef3bc181ffb785d9b7af4840e7
Tutul Kabir <tutulkabir@gmail.com>
2023-01-21 22:32:25 +0530

    updated Netodo.js file and handling form

--- src/components/NewTodo.js
+++ src/components/NewTodo.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./newTodo.module.css";
 const NewTodo = () => {
+  const [todo, setTodo] = useState({ title: "", desc: "" });
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    setTodo((oldTodo) => {
+      return { ...oldTodo, [name]: event.target.value };
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("form is submitted");
+    console.log(todo);
+    setTodo({ title: "", desc: "" });
+  };
   return (
-    <form className={style.form}>
+    <form className={style.form} onSubmit={handleSubmit}>
       <div className={style["form-field"]}>
         <label htmlFor="title">Title:</label>
-        <input type="text" id="title" name="title" />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={todo.title}
+          onChange={handleChange}
+        />
       </div>
       <div className={style["form-field"]}>
         <label htmlFor="desc">Description:</label>
-        <textarea name="desc" id="desc"></textarea>
+        <textarea
+          name="desc"
+          id="desc"
+          value={todo.desc}
+          onChange={handleChange}
+        ></textarea>
       </div>
       <button type="submit">Add Todo</button>
     </form>
